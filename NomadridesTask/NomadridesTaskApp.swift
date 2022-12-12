4389f437ad24c9fce9ae3cca9094deb878bfbbae
lokeshkumawat <lkumawat@innominds.com>
2022-12-12 21:26:27 +0530

    Initial Commit

---
+++ NomadridesTask/NomadridesTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomadridesTaskApp.swift
+//  NomadridesTask
+//
+//  Created by IM-LP-1819 on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NomadridesTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
